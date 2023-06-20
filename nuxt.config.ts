5be16fb06ba1295357223f5c8a9cac23a7c6bc13
Bravco <boss.wiw@gmail.com>
2023-06-20 14:33:16 +0200

    Integrated Supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   modules: [
     "nuxt-icon",
     "nuxt-swiper",
+    "@nuxtjs/supabase",
   ],
   css: [
     "~/assets/main.css",
--- pages/ponuka.vue
+++ pages/ponuka.vue
@@ -5,6 +5,17 @@
     </section>
 </template>
 
+<script setup>
+    const client = useSupabaseClient();
+
+    const { data } = await useAsyncData("pizzas", async () => {
+        const { data } = (await client.from("pizzas").select());
+        return data;
+    });
+
+    console.log(data);
+</script>
+
 <style scoped>
     .hero-background {
         background-image: url("~/assets/hero-ponuka.png");
