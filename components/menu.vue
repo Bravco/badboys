3f12f6bbca3c9ffde252d2831a96a982a9542b41
Bravco <boss.wiw@gmail.com>
2023-06-22 07:05:29 +0200

    Made menu component

---
+++ components/menu.vue
@@ -0,0 +1,123 @@
+<template>
+    <section class="menu-section">
+        <div class="menu-header">
+            <h2>{{ title }}</h2>
+            <div class="menu-description paragraph-alt">
+                <slot name="description"></slot>
+            </div>
+        </div>
+        <ul class="menu-list">
+            <li v-for="item in items" class="menu-item">
+                <img class="menu-item-image" src="~/assets/pizza.svg" alt="pizza">
+                <div class="menu-item-info">
+                    <div class="text">
+                        <h4>
+                            {{ item.title }}
+                            <Icon v-if="item.isVegetarian" class="vegetarian-icon" name="fa6-solid:leaf" size="1.5rem"/>
+                            <Icon v-if="item.isSpicy" class="spicy-icon" name="fa6-solid:pepper-hot" size="1.5rem"/>
+                        </h4>
+                        <p>
+                            {{ item.description }}
+                            <span v-if="item.alergens" class="alergens">({{ item.alergens }})</span>
+                        </p>
+                    </div>
+                    <h4 class="price">
+                        {{ item.miniPrice?.toFixed(2) }}
+                        <span v-if="item.miniPrice">/</span>
+                        {{ item.normalPrice.toFixed(2) }}
+                        <span v-if="item.maxiPrice">/</span>
+                        {{ item.maxiPrice?.toFixed(2) }}
+                        <span>€</span>
+                    </h4>
+                </div>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+    const props = defineProps({
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        items: {
+            type: Array,
+            required: true,
+        },
+    });
+</script>
+
+<style scoped>
+    .menu-section {
+        justify-content: center;
+        align-content: start;
+        gap: 4rem;
+        padding-bottom: 10%;
+    }
+
+    .menu-header {
+        display: grid;
+        place-items: center;
+        gap: 1rem;
+    }
+
+    .menu-description {
+        display: flex;
+        gap: 2rem;
+        font-weight: 500;
+    }
+
+    .menu-list {
+        display: grid;
+        grid-template-columns: repeat(2, 32rem);
+        gap: 2rem;
+    }
+
+    .menu-item {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .menu-item-image {
+        width: 6rem;
+        filter: invert(1) opacity(50%);
+    }
+
+    .menu-item-info {
+        display: grid;
+        gap: 1rem;
+    }
+
+    .menu-item-info .text {
+        display: grid;
+        gap: .25rem;
+    }
+
+    .price {
+        color: var(--color-primary);
+    }
+
+    .price span, .alergens {
+        color: var(--color-text-alt);
+    }
+
+    .vegetarian-icon {
+        color: #4EFF4A;
+    }
+
+    .spicy-icon {
+        color: red;
+    }
+
+    @media only screen and (max-width: 1024px) {
+        .menu-list {
+            grid-template-columns: 32rem;
+        }
+    }
+</style>
--- pages/ponuka.vue
+++ pages/ponuka.vue
@@ -4,42 +4,19 @@
         <div class="hero-background"></div>
     </section>
     <Quickinfo/>
-    <section class="menu-section">
-        <div class="menu-header">
-            <h2>Pizza</h2>
-            <div class="menu-description paragraph-alt">
-                <p>MINI - ∅ 20 cm, 300 g</p>
-                <p>CLASSIC - ∅ 30 cm, 500 g</p>
-                <p>MAXI - ∅ 50 cm, 900 g</p>
-            </div>
-        </div>
-        <ul class="menu-list">
-            <li v-for="pizza in pizzas" class="menu-item">
-                <img class="menu-item-image" src="~/assets/pizza.svg" alt="pizza">
-                <div class="menu-item-info">
-                    <div class="text">
-                        <h4>
-                            {{ pizza.title }}
-                            <Icon v-if="pizza.isVegetarian" class="vegetarian-icon" name="fa6-solid:leaf" size="1.5rem"/>
-                            <Icon v-if="pizza.isSpicy" class="spicy-icon" name="fa6-solid:pepper-hot" size="1.5rem"/>
-                        </h4>
-                        <p>
-                            {{ pizza.description }}
-                            <span class="alergens">({{ pizza.alergens }})</span>
-                        </p>
-                    </div>
-                    <h4 class="price">
-                        {{ pizza.miniPrice?.toFixed(2) }}
-                        <span v-if="pizza.miniPrice">/</span>
-                        {{ pizza.normalPrice.toFixed(2) }}
-                        <span v-if="pizza.maxiPrice">/</span>
-                        {{ pizza.maxiPrice?.toFixed(2) }}
-                        <span>€</span>
-                    </h4>
-                </div>
-            </li>
-        </ul>
-    </section>
+    <Menu title="Pizza" :items="pizzas">
+        <template v-slot:description>
+            <p>MINI - ∅ 20 cm, 300 g</p>
+            <p>CLASSIC - ∅ 30 cm, 500 g</p>
+            <p>MAXI - ∅ 50 cm, 900 g</p>
+        </template>
+    </Menu>
+
+    <Menu title="Štangle" :items="stangle">
+        <template v-slot:description>
+            <p>DRESING - 140 g : klasik, cesnakový, pikantný</p>
+        </template>
+    </Menu>
 </template>
 
 <script setup>
@@ -49,71 +26,30 @@
         const { data } = (await client.from("pizzas").select().order("id"));
         return data;
     });
+
+    const { data: stangle } = await useAsyncData("stangle", async () => {
+        const { data } = (await client.from("stangle").select().order("id"));
+        return data;
+    });
+
+    const { data: salads } = await useAsyncData("salads", async () => {
+        const { data } = (await client.from("salads").select().order("id"));
+        return data;
+    });
+
+    const { data: pasta } = await useAsyncData("pasta", async () => {
+        const { data } = (await client.from("pasta").select().order("id"));
+        return data;
+    });
+
+    const { data: others } = await useAsyncData("others", async () => {
+        const { data } = (await client.from("others").select().order("id"));
+        return data;
+    });
 </script>
 
 <style scoped>
     .hero-background {
         background-image: url("~/assets/hero-ponuka.png");
-    }
-
-    .menu-section {
-        justify-content: center;
-        align-content: start;
-        gap: 4rem;
-        padding-bottom: 10%;
-    }
-
-    .menu-header {
-        display: grid;
-        place-items: center;
-        gap: 1rem;
-    }
-
-    .menu-description {
-        display: flex;
-        gap: 2rem;
-    }
-
-    .menu-list {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-        gap: 2rem;
-    }
-
-    .menu-item {
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-    }
-
-    .menu-item-image {
-        width: 6rem;
-        filter: invert(1) opacity(50%);
-    }
-
-    .menu-item-info {
-        display: grid;
-        gap: 1rem;
-    }
-
-    .menu-item-info .text {
-        display: grid;
-        gap: .25rem;
-    }
-
-    .price {
-        color: var(--color-primary);
-    }
-
-    .price span, .alergens {
-        color: var(--color-text-alt);
-    }
-
-    .vegetarian-icon {
-        color: #4EFF4A;
-    }
-
-    .spicy-icon {
-        color: red;
     }
 </style>
