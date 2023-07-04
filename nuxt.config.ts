55538cc80e8c4bd0a80c8f3ee965f39195913a57
Bravco <boss.wiw@gmail.com>
2023-07-04 15:57:07 +0200

    Configured for client-side only rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
+  nitro: {
+    preset: "service-worker",
+  },
   modules: [
     "nuxt-icon",
     "nuxt-swiper",
